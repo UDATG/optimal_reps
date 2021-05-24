f46d044cac522d45b68f8f446ebbb07a0b73bd73
luli2949 <13612124718@163.com>
2021-05-24 10:17:31 -0500

    linear solver working

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,67 @@ use exhact::chx::{ChainComplex, factor_chain_complex, ChxTransformKind};
 use exhact::clique::Simplex;
 use num::rational::Ratio;
 use std;
+use coin_cbc::{raw::Status, Col, Model, Sense, Solution as CbcSolution};
+use sprs::{CsMat, CsVec};
+use std::collections::HashMap;
+
 
 
 fn main() {
+    // Create the problem.
+    let mut m = Model::default();
+    m.set_parameter("log", "0"); // turn off logging 
+    let obj_coef = vec![50., 120.]; // c^T
+    let cols: Vec<Col> = obj_coef.clone()
+        .into_iter()
+        .map(
+            |x| {
+                let col = m.add_col();
+                if false {
+                    m.set_integer(col);
+                }
+                col
+            },
+        )
+        .collect(); // x 
+    for i in 0..obj_coef.len(){
+        m.set_obj_coeff(cols[i],obj_coef[i]); // setting up the objective function 
+    }
+    let A = CsMat::new((3, 2),
+                       vec![0,2,4,6],
+                       vec![0, 1, 0, 1, 0,1],
+                       vec![100.,200.,10.,30.,1.,1.]); // matrix A    
+    let upper = vec![10000.,1200.,110.]; // b
+    // Ax <= b
+    for i in 0..(A.rows()){
+        let row = m.add_row();
+        m.set_row_upper(row,upper[i]);
+        for j in A.proper_indptr()[i]..(A.proper_indptr()[i+1]){
+            m.set_weight(row,cols[A.indices()[j]] , A.data()[j]);
+        }
+    } 
+    // Set objective sense.
+    m.set_obj_sense(Sense::Maximize);
 
+    // Solve the problem. Returns the solution
+    let sol = m.solve();
+ 
+    println!("{:?}", sol.col(cols[0]));
+    println!("{:?}", sol.col(cols[1]));
 
-
-    // ----------------------------------------------------------------------------------
-    // Set maximum threshold values for homology dimension and dissimilarity
-    // ----------------------------------------------------------------------------------
-    let dim = 3;
+    let dim = 1;
     let maxdis = 2;
-    
-    
-    // ----------------------------------------------------------------------------------
-    // Define an object to represent the ring Z/3Z
-    // ----------------------------------------------------------------------------------
+ 
     let ringmetadata = exhact::matrix::RingMetadata{
-    	ringspec: RingSpec::Rational,
-    	identity_additive: Ratio::new(0, 1),
-    	identity_multiplicative: Ratio::new(1, 1)
+     ringspec: RingSpec::Rational,
+     identity_additive: Ratio::new(0, 1),
+     identity_multiplicative: Ratio::new(1, 1)
     };
-    
 
-    // ----------------------------------------------------------------------------------
-    // Build a "dissimilarity matrix" as a vector of vectors
-    // ----------------------------------------------------------------------------------
     let dismat = vec![  vec![0,  1,  2,  1],
                         vec![1,  0,  1,  2],
                         vec![2,  1,  0,  1],
                         vec![1,  2,  1,  0]  ];
-    
-    
-    // ----------------------------------------------------------------------------------
-    // Construct the corresponding filtered clique complex
-    // ----------------------------------------------------------------------------------
     let chx = exhact::clique::CliqueComplex {
         // the distance/dissimilarity matrix
         dissimilarity_matrix: dismat, 
@@ -54,158 +80,46 @@ fn main() {
         // don't worry about this
         simplex_count: Vec::new() 
     };
-    
 
-    // ----------------------------------------------------------------------------------
-    // Get a (row-major) sparse matrix oracle for the boundary operator
-    // ----------------------------------------------------------------------------------
-    let D =  chx.get_smoracle(exhact::matrix::MajorDimension::Row,
-                              exhact::chx::ChxTransformKind::Boundary);
-    
-   
-    // ----------------------------------------------------------------------------------
-    // Define a weighted 0-simplex and 1-simplex
-    // ----------------------------------------------------------------------------------
-    let simplex_d1 = exhact::clique::Simplex{
-        filvalue: 1,
-        vertices : vec![0, 1]
-    };
-    let simplex_d0 = exhact::clique::Simplex{
-        filvalue: 0,
-        vertices : vec![0]
-    };
-
-
-    // ----------------------------------------------------------------------------------
-    // Compute / check the filtration values
-    // ----------------------------------------------------------------------------------
-    std::assert_eq!(chx.key_2_filtration( &simplex_d0 ), 0);
-    std::assert_eq!(chx.key_2_filtration( &simplex_d1 ), 1);
-
-
-    // ----------------------------------------------------------------------------------
-    // Access a row of the boundary matrix 
-    //  - the boundary matrix is "row-major" so we call rows "major fields"
-    // ----------------------------------------------------------------------------------
-    // create an iterator that runs over the structural nonzero entries of the row
-    // each item returned by the iterator is a tuple of form (weighted_simplex, coefficient)
-    let major_field = D.maj_itr( &simplex_d0 );
-
-    
-    // the following for-loop should print the following:
-    // ```text
-    // Structural nonzero entries of a row corresponding to a 0-simplex:
-    // (Simplex { filvalue: 1, vertices: [0, 1] }, -1)
-    // (Simplex { filvalue: 2, vertices: [0, 2] }, -1)
-    // (Simplex { filvalue: 1, vertices: [0, 3] }, -1)
-    // ```
-    println!("Structural nonzero entries of a row corresponding to a 0-simplex:");
-    for item in major_field  {
-        println!("{:?}", item);
-    }
-    
-    // check to ensure that the output is correct:
-    let mut correct_val : Vec< (Simplex<i64>, i16) >  = 
-                      vec![ (Simplex{ filvalue: 1, vertices: vec![0, 1] }, -1),
-                            (Simplex{ filvalue: 2, vertices: vec![0, 2] }, -1),
-                            (Simplex{ filvalue: 1, vertices: vec![0, 3] }, -1) ];
-    
-    let major_field2 = D.maj_itr( &simplex_d0 ); // this re-creates the iterator
-    std::assert_eq!( major_field2.eq(correct_val.iter().map( |x| x.clone() ) ), true);
-    
-
-    // ----------------------------------------------------------------------------------
-    // Access a column of the boundary matrix 
-    //  - the boundary matrix is "row-major" so we call columns "column fields"
-    // ----------------------------------------------------------------------------------
-    // create an iterator that runs over the structural nonzero entries of the row
-    // each item returned by the iterator is a tuple of form (weighted_simplex, coefficient)
-    let minor_field = D.min_itr( &simplex_d1 );
-    
-    // the following for-loop should print the following:
-    // ```text
-    // Structural nonzero entries of a column corresponding to a 1-simplex:
-    // (Simplex { filvalue: 0, vertices: [1] }, 1)
-    // (Simplex { filvalue: 0, vertices: [0] }, -1)
-    // ```
-    println!("Structural nonzero entries of a column corresponding to a 1-simplex:");
-    for item in minor_field  {
-        println!("{:?}", item);
-    }
-
-    // check to ensure that the output is correct:
-    let mut correct_val : Vec< (Simplex<i64>, i16) >  = 
-                      vec![  (Simplex{ filvalue: 0, vertices: vec![1] },  1),
-                             (Simplex{ filvalue: 0, vertices: vec![0] }, -1)  ];
-    
-    let minor_field2 = D.min_itr( &simplex_d1 ); // this re-creates the iterator
-    std::assert_eq!( minor_field2.eq(correct_val.iter().map( |x| x.clone() ) ), true);
-
-
-    // ----------------------------------------------------------------------------------
-    // Factor the complex 
-    // ----------------------------------------------------------------------------------
     let factored_complex = exhact::chx::factor_chain_complex(&chx, dim+1);
 
 
-    // ----------------------------------------------------------------------------------
-    // Read barcodes / check for correctness
-    // ----------------------------------------------------------------------------------
-    // predefine the set of correct solutions
-    let correct_barcodes = vec![    vec![ (0, 2), (0, 1), (0, 1), (0, 1) ], // dimension 0
-                                    vec![ (1, 2) ],                         // dimension 1
-                                    vec![],                                 // dimension 2
-                                    vec![]                                  // dimension 3
-                                ];
-   
-    // confirm that correct solutions and actual solutions are the same
-    for i in 0..dim+1 {
-        assert_eq!( correct_barcodes[i], factored_complex.barcode(i) );
+    for i in 1..(1+1){
+        //record as simplices
+        let simplex_bar = factored_complex.simplex_barcode(i);
+        let birth = &simplex_bar[0].0;
+        let death = &simplex_bar[0].1;
+        // loop over Sn
+        let Fn = chx.keys_unordered_itr(i).filter(|s| s <= &death );
+        // loop over S_{n+1}
+        let Fn1 = chx.keys_unordered_itr(i+1).filter(|s| s <= &death && s>=&birth);
+        // build oracle for the entire boundary matrix
+        let D =  chx.get_smoracle(exhact::matrix::MajorDimension::Row,
+                              exhact::chx::ChxTransformKind::Boundary);
+        // create hashmaps to store keys to indices 
+        let mut maj_2_index = HashMap::new();       
+        let mut min_2_index = HashMap::new();   
+        // initialize indices to be 0   
+        let maj_index = 0;
+        let min_index = 0;
+        // create sparse matrix
+        for edge in Fn{
+            if !maj_2_index.contains_key(&edge) {
+                maj_2_index.insert(edge.clone(), maj_index); 
+                let maj_index = maj_index + 1;
+            }
+            let minor_fields = D.maj_itr(&edge);
+            for minor_field in minor_fields{
+                let tri = minor_field.0;
+                let data = minor_field.1;
+                if !min_2_index.contains_key(&tri) {
+                    min_2_index.insert(tri.clone(), min_index); 
+                    let min_index = min_index + 1;
+                }
+                println!("c");
+                println!("{:?}", tri);
+                println!("{:?}", data);
+            }
+        }
     }
-
-
-    // ----------------------------------------------------------------------------------
-    // Get the cycle representative associated with a (weighted) simplex
-    //    - this may corresopnd to a "length-0 bar", depending on choice
-    // ----------------------------------------------------------------------------------
-    // get the vector *represented as a hashmap*
-    let basis_vec = factored_complex.get_matched_basis_vector(1, &simplex_d1);
-
-    // this for-loop should print the following:
-    // ```text
-    // Basis vector corresponding to a 1-simplex: 
-    // (Simplex { filvalue: 1, vertices: [0, 1] }, 1) 
-    // ```
-    println!("Basis vector corresponding to a 1-simplex:");
-    for item in basis_vec.iter()  {
-        println!("{:?}", item);
-    }
-
-    // check to ensure that the output is correct:
-        // write a vector with correct values
-    let mut correct_val : Vec< (Simplex<i64>, i16) >  = 
-                      vec![  (Simplex{ filvalue: 1, vertices: vec![0, 1] },  1)  ];
-    
-        // convert the hashmap to an iterator  
-        
-        // remark: if we had tried to create this iterator via a command
-        // > factored_complex.get_matched_basis_vector(1, &simplex_d1).iter().map( etc. )
-        // then we would have gotten an error; the reason is that by declaring the `basis_vec`
-        // variable name we signaled rust that it should have a certain lifetime; see 
-        // https://stackoverflow.com/questions/54056268/temporary-value-is-freed-at-the-end-of-this-statement/54056716#54056716
-        // for further discussion
-    let basis_vec_iter = basis_vec
-                            .iter() // get iterator for the hashmap
-                            .map( |x| ( x.0.clone(), x.1.clone() ) ); // replace ref's with clones
-
-        // check equality
-    std::assert_eq!( basis_vec_iter.eq( correct_val.iter().cloned() ) , true);
-
-
-    // ----------------------------------------------------------------------------------
-    // Get the birth and death filtraitons of a chain 
-    //    - here "chain" is formalized as a hashmap mapping keys to coefficients
-    // ----------------------------------------------------------------------------------
-    // this part is not implemented yet.
 }
-
