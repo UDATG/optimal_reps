e1cb4a8c0da5901186cf472115f5fe9e197def15
luli2949 <13612124718@163.com>
2021-05-26 16:01:13 -0500

    current code with bugs

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,210 @@
 use exhact::matrix::{SmOracle, RingSpec, RingMetadata, MajorDimension};
 use exhact::cubical::{Cube, CubicalComplex};
-use exhact::chx::{ChainComplex, factor_chain_complex, ChxTransformKind};
+use exhact::chx::{ChainComplex, factor_chain_complex, ChxTransformKind,FactoredComplexBlockCsm};
 use exhact::clique::Simplex;
 use num::rational::Ratio;
 use std;
 use coin_cbc::{raw::Status, Col, Model, Sense, Solution as CbcSolution};
 use sprs::{CsMat, CsVec};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::hash::Hash;
+
+// must add the following line to dependencies under Cargo.toml:
+//      ordered-float = "2.0"
+
+use ordered_float::OrderedFloat;
+
+type Coefficient = Ratio<i32>;
 
 
 
-fn main() {
-    // Create the problem.
-    let mut m = Model::default();
-    m.set_parameter("log", "0"); // turn off logging 
-    let obj_coef = vec![50., 120.]; // c^T
-    let cols: Vec<Col> = obj_coef.clone()
-        .into_iter()
-        .map(
-            |x| {
-                let col = m.add_col();
-                if false {
-                    m.set_integer(col);
-                }
-                col
-            },
-        )
-        .collect(); // x 
-    for i in 0..obj_coef.len(){
-        m.set_obj_coeff(cols[i],obj_coef[i]); // setting up the objective function 
-    }
-    let A = CsMat::new((3, 2),
-                       vec![0,2,4,6],
-                       vec![0, 1, 0, 1, 0,1],
-                       vec![100.,200.,10.,30.,1.,1.]); // matrix A    
-    let upper = vec![10000.,1200.,110.]; // b
-    // Ax <= b
-    for i in 0..(A.rows()){
-        let row = m.add_row();
-        m.set_row_upper(row,upper[i]);
-        for j in A.proper_indptr()[i]..(A.proper_indptr()[i+1]){
-            m.set_weight(row,cols[A.indices()[j]] , A.data()[j]);
+fn ordered_floats( v : Vec<f64> ) -> Vec< OrderedFloat<f64> > {
+    let u : Vec<_> = v.into_iter().map(OrderedFloat).collect(); 
+    return u
+}
+
+fn ordered_floats_nested( v: Vec< Vec< f64 > > ) -> Vec< Vec< OrderedFloat<f64> > > {
+    
+    return v.into_iter().map( ordered_floats ).collect();
+
+}
+
+
+fn tri_opt<'a, MatrixIndexKey, Filtration, OriginalChx, Matrix>
+(
+    is_pos:bool, // optimize over the positive domain
+    is_int:bool,
+    factored_complex: FactoredComplexBlockCsm<'a, MatrixIndexKey, Coefficient, Filtration, OriginalChx>,
+    chx:  ChainComplex<MatrixIndexKey, Coefficient, Filtration, Matrix=Matrix>)-> Vec<f64>
+    where   OriginalChx: ChainComplex<MatrixIndexKey, Coefficient, Filtration, Matrix=Matrix>,
+            MatrixIndexKey: PartialEq+ Eq + Clone + Hash,
+            Matrix: SmOracle<MatrixIndexKey, MatrixIndexKey, Coefficient>
+
+
+    {
+
+    let mut barcode = factored_complex.simplex_barcode(1);
+    barcode.sort();
+ // println!("Barcode contains {} bars.", barcode.len());
+    // for (start, end) in barcode.iter() {
+    //     println!("{:?},{:?}", &start, &end);
+    // }
+
+    //     let dismat = vec![  vec![0.,  1.,  2.,  1.],
+    //                     vec![1., 0.,  1.,  2.],
+    //                     vec![2.,  1.,  0.,  1.],
+    //                     vec![1.,  2.,  1.,  0.]  ];
+    // let dismat = ordered_floats_nested(dismat);
+
+    for i in 1..(1+1){
+        let mut m = Model::default();
+        m.set_parameter("log", "0"); // turn off logging 
+        // a list of tuples (birth simplex, death simplex)
+        let simplex_bar = factored_complex.simplex_barcode(i);
+
+
+        let birth = &simplex_bar[0].0;
+        let death = &simplex_bar[0].1;
+
+
+        // loop over Sn+1
+        let Fn1 = chx.keys_unordered_itr(i+1).filter(|s| s <= &death && s>=&birth );
+        let size = Fn1.count();
+        // loop over Sn
+        let Fn = chx.keys_unordered_itr(i).filter(|s| s <= &death && s>=&birth);
+
+        let obj_coef = vec![1.; 2*size]; // c^T // 1 vector with length |Fn|
+        let cols: Vec<Col> = obj_coef.clone()
+            .into_iter()
+            .map(
+                |x| {
+                    let col = m.add_col();
+                    if is_int {
+                        m.set_integer(col);
+                    }
+                    col
+                },
+            )
+            .collect(); // x 
+    
+        // println!("HERE");
+        // println!("{:?}",cols.len());
+        for i in 0..obj_coef.len(){
+            m.set_obj_coeff(cols[i],obj_coef[i]); // setting up the objective function 
         }
-    } 
-    // Set objective sense.
-    m.set_obj_sense(Sense::Maximize);
 
-    // Solve the problem. Returns the solution
-    let sol = m.solve();
- 
-    println!("{:?}", sol.col(cols[0]));
-    println!("{:?}", sol.col(cols[1]));
+  
+        // build oracle for the entire boundary matrix
+        let D =  chx.get_smoracle(exhact::matrix::MajorDimension::Row,
+                              exhact::chx::ChxTransformKind::Boundary);
+        // create hashmaps to store keys to indices 
+        let mut maj_2_index: HashMap<exhact::clique::Simplex<OrderedFloat<f64>>, usize> = HashMap::new();       
+        let mut min_2_index: HashMap<exhact::clique::Simplex<OrderedFloat<f64>>, usize> = HashMap::new();   
+        // initialize indices to be 0   
+        let mut maj_index:usize = 0;
+        let mut min_index:usize = 0;
+        // create sparse matrix
+        
+        //let pos = true;
 
+        let mut ind_ptr = Vec::new();
+        ind_ptr.push(0);
+        let mut col_ind = Vec::new();
+        let mut nz_val = Vec::new();
+
+        let mut counter = 1;
+
+        for edge in Fn{ // for each row 
+
+            let row = m.add_row();
+            if edge == *birth {
+                if is_pos{
+                    m.set_row_lower(row,f64::EPSILON);
+                }
+                else{
+                    m.set_row_upper(row,-f64::EPSILON);
+                }
+            }
+            else{
+                m.set_row_upper(row, 0.0);
+                m.set_row_lower(row, 0.0);
+            }
+
+            if !maj_2_index.contains_key(&edge) {
+                maj_2_index.insert(edge.clone(), maj_index); 
+                maj_index = maj_index + 1;
+            }
+            let minor_fields = D.maj_itr(&edge);
+            for minor_field in minor_fields{
+                // column index (S_{n+1})
+                let tri = minor_field.0;
+                // entry value
+                let data = minor_field.1;
+                if &tri <=death && &tri>= birth{
+                    
+                    if !min_2_index.contains_key(&tri) {
+                        min_2_index.insert(tri.clone(), min_index);
+                        min_index = min_index + 1;
+                    }
+                    col_ind.push(*min_2_index.get(&tri).unwrap());
+                    nz_val.push((data.numer()/data.denom()).into());
+                    counter = counter+1;
+                    m.set_weight(row,cols[*min_2_index.get(&tri).unwrap()] , (data.numer()/data.denom()).into());
+                    m.set_weight(row,cols[*min_2_index.get(&tri).unwrap()+size] , ((-data.numer()/data.denom()).into()));
+                    
+                }
+            }
+            ind_ptr.push(counter);
+        }
+            // Set objective sense.
+        m.set_obj_sense(Sense::Minimize);
+
+        //min_2_index.get(&death): index corresponding to tau
+        m.set_col_upper(cols[*min_2_index.get(&death).unwrap()],1.0);
+        m.set_col_lower(cols[*min_2_index.get(&death).unwrap()],1.0);
+        m.set_col_upper(cols[*min_2_index.get(&death).unwrap()+size],0.0);
+        m.set_col_lower(cols[*min_2_index.get(&death).unwrap()+size],0.0);
+
+    
+        // Solve the problem. Returns the solution
+        let sol = m.solve();
+
+        let mut v= Vec::with_capacity(size);
+        for i in 0..size{
+            v.push(sol.col(cols[i])-sol.col(cols[i+size]));
+        }
+
+        return v;
+
+    }
+}
+
+
+fn main() {    
+    // read file
+    let mut f = BufReader::new(File::open("/Users/lizhaoheng/Desktop/optimal_reps-main/senate104_edge_list.txt_0.68902_distmat.txt").unwrap());
+    let mut s = String::new();
+
+    let arr: Vec<Vec<f64>> = f.lines()
+        .map(|l| l.unwrap().split(char::is_whitespace)
+             .map(|number| number.parse().unwrap())
+             .collect())
+        .collect();
+    println!("{:?}", arr.len());
+    let dismat = ordered_floats_nested(arr);
+    println!("{:?}", dismat.clone().len());
     let dim = 1;
-    let maxdis = 2;
- 
+    const maxdis: OrderedFloat<f64> = OrderedFloat(1.);
+
     let ringmetadata = exhact::matrix::RingMetadata{
      ringspec: RingSpec::Rational,
      identity_additive: Ratio::new(0, 1),
      identity_multiplicative: Ratio::new(1, 1)
     };
 
-    let dismat = vec![  vec![0,  1,  2,  1],
-                        vec![1,  0,  1,  2],
-                        vec![2,  1,  0,  1],
-                        vec![1,  2,  1,  0]  ];
+
     let chx = exhact::clique::CliqueComplex {
         // the distance/dissimilarity matrix
         dissimilarity_matrix: dismat, 
@@ -80,46 +220,155 @@ fn main() {
         // don't worry about this
         simplex_count: Vec::new() 
     };
-
+    
     let factored_complex = exhact::chx::factor_chain_complex(&chx, dim+1);
 
+    let v = tri_opt(true,true,factored_complex, chx);
 
-    for i in 1..(1+1){
-        //record as simplices
-        let simplex_bar = factored_complex.simplex_barcode(i);
-        let birth = &simplex_bar[0].0;
-        let death = &simplex_bar[0].1;
-        // loop over Sn
-        let Fn = chx.keys_unordered_itr(i).filter(|s| s <= &death );
-        // loop over S_{n+1}
-        let Fn1 = chx.keys_unordered_itr(i+1).filter(|s| s <= &death && s>=&birth);
-        // build oracle for the entire boundary matrix
-        let D =  chx.get_smoracle(exhact::matrix::MajorDimension::Row,
-                              exhact::chx::ChxTransformKind::Boundary);
-        // create hashmaps to store keys to indices 
-        let mut maj_2_index = HashMap::new();       
-        let mut min_2_index = HashMap::new();   
-        // initialize indices to be 0   
-        let maj_index = 0;
-        let min_index = 0;
-        // create sparse matrix
-        for edge in Fn{
-            if !maj_2_index.contains_key(&edge) {
-                maj_2_index.insert(edge.clone(), maj_index); 
-                let maj_index = maj_index + 1;
-            }
-            let minor_fields = D.maj_itr(&edge);
-            for minor_field in minor_fields{
-                let tri = minor_field.0;
-                let data = minor_field.1;
-                if !min_2_index.contains_key(&tri) {
-                    min_2_index.insert(tri.clone(), min_index); 
-                    let min_index = min_index + 1;
-                }
-                println!("c");
-                println!("{:?}", tri);
-                println!("{:?}", data);
-            }
-        }
-    }
+
+//     let mut barcode = factored_complex.simplex_barcode(1);
+//     barcode.sort();
+//  // println!("Barcode contains {} bars.", barcode.len());
+//     // for (start, end) in barcode.iter() {
+//     //     println!("{:?},{:?}", &start, &end);
+//     // }
+
+//     //     let dismat = vec![  vec![0.,  1.,  2.,  1.],
+//     //                     vec![1., 0.,  1.,  2.],
+//     //                     vec![2.,  1.,  0.,  1.],
+//     //                     vec![1.,  2.,  1.,  0.]  ];
+//     // let dismat = ordered_floats_nested(dismat);
+
+//     for i in 1..(1+1){
+//         let mut m = Model::default();
+//         m.set_parameter("log", "0"); // turn off logging 
+//         // a list of tuples (birth simplex, death simplex)
+//         let simplex_bar = factored_complex.simplex_barcode(i);
+
+
+//         let birth = &simplex_bar[0].0;
+//         let death = &simplex_bar[0].1;
+
+
+//         // loop over Sn+1
+//         let Fn1 = chx.keys_unordered_itr(i+1).filter(|s| s <= &death && s>=&birth );
+//         let size = Fn1.count();
+//         // loop over Sn
+//         let Fn = chx.keys_unordered_itr(i).filter(|s| s <= &death && s>=&birth);
+
+//         let obj_coef = vec![1.; 2*size]; // c^T // 1 vector with length |Fn|
+//         let cols: Vec<Col> = obj_coef.clone()
+//             .into_iter()
+//             .map(
+//                 |x| {
+//                     let col = m.add_col();
+//                     if false {
+//                         m.set_integer(col);
+//                     }
+//                     col
+//                 },
+//             )
+//             .collect(); // x 
+    
+//         // println!("HERE");
+//         // println!("{:?}",cols.len());
+//         for i in 0..obj_coef.len(){
+//             m.set_obj_coeff(cols[i],obj_coef[i]); // setting up the objective function 
+//         }
+
+  
+//         // build oracle for the entire boundary matrix
+//         let D =  chx.get_smoracle(exhact::matrix::MajorDimension::Row,
+//                               exhact::chx::ChxTransformKind::Boundary);
+//         // create hashmaps to store keys to indices 
+//         let mut maj_2_index: HashMap<exhact::clique::Simplex<OrderedFloat<f64>>, usize> = HashMap::new();       
+//         let mut min_2_index: HashMap<exhact::clique::Simplex<OrderedFloat<f64>>, usize> = HashMap::new();   
+//         // initialize indices to be 0   
+//         let mut maj_index:usize = 0;
+//         let mut min_index:usize = 0;
+//         // create sparse matrix
+        
+//         let pos = true;
+
+//         let mut ind_ptr = Vec::new();
+//         ind_ptr.push(0);
+//         let mut col_ind = Vec::new();
+//         let mut nz_val = Vec::new();
+
+//         let mut counter = 1;
+
+//         for edge in Fn{ // for each row 
+
+//             let row = m.add_row();
+//             if edge == *birth {
+//                 if pos{
+//                     m.set_row_lower(row,f64::EPSILON);
+//                 }
+//                 else{
+//                     m.set_row_upper(row,-f64::EPSILON);
+//                 }
+//             }
+//             else{
+//                 m.set_row_upper(row, 0.0);
+//                 m.set_row_lower(row, 0.0);
+//             }
+
+//             if !maj_2_index.contains_key(&edge) {
+//                 maj_2_index.insert(edge.clone(), maj_index); 
+//                 maj_index = maj_index + 1;
+//             }
+//             let minor_fields = D.maj_itr(&edge);
+//             for minor_field in minor_fields{
+//                 // column index (S_{n+1})
+//                 let tri = minor_field.0;
+//                 // entry value
+//                 let data = minor_field.1;
+//                 if &tri <=death && &tri>= birth{
+                    
+//                     if !min_2_index.contains_key(&tri) {
+//                         min_2_index.insert(tri.clone(), min_index);
+//                         min_index = min_index + 1;
+//                     }
+//                     col_ind.push(*min_2_index.get(&tri).unwrap());
+//                     nz_val.push((data.numer()/data.denom()).into());
+//                     counter = counter+1;
+//                     m.set_weight(row,cols[*min_2_index.get(&tri).unwrap()] , (data.numer()/data.denom()).into());
+//                     m.set_weight(row,cols[*min_2_index.get(&tri).unwrap()+size] , ((-data.numer()/data.denom()).into()));
+                    
+//                 }
+//             }
+//             ind_ptr.push(counter);
+//         }
+//             // Set objective sense.
+//         m.set_obj_sense(Sense::Minimize);
+
+//         //min_2_index.get(&death): index corresponding to tau
+//         m.set_col_upper(cols[*min_2_index.get(&death).unwrap()],1.0);
+//         m.set_col_lower(cols[*min_2_index.get(&death).unwrap()],1.0);
+//         m.set_col_upper(cols[*min_2_index.get(&death).unwrap()+size],0.0);
+//         m.set_col_lower(cols[*min_2_index.get(&death).unwrap()+size],0.0);
+
+    
+//         // Solve the problem. Returns the solution
+//         let sol = m.solve();
+    
+        // println!("{:?}", sol.col(cols[0]));
+        // println!("{:?}", sol.col(cols[1]));
+
+        // let mut v= Vec::with_capacity(size);
+        // for i in 0..size{
+        //     v.push(sol.col(cols[i])-sol.col(cols[i+size]));
+        // }
+
+
+        // let a = CsMat::new((Fn.count(), size),
+        //                ind_ptr,
+        //                col_ind,
+        //                nz_val);
+
+        
+
+
+    
+    //}
 }
